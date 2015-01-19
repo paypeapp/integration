9a7b066688ffef1347d065121e975e22f3974103
robertpallas <robert.pallas@igame.com>
2015-01-19 18:41:30 +0200

    email interface added - sending new customers to inbox

---
+++ wsInterfaces/email.php
@@ -0,0 +1,57 @@
+<?php
+class Email implements WsInterface
+{
+	private $emails;
+
+	public function __construct($wsConfig, $paypeApi)
+	{
+		$this->emails = $wsConfig['emails'];
+	}
+
+	public function getCustomers($lastSyncCustomerId)
+	{
+		// No GET from email
+		return;
+	}
+
+	// format customers and send to emails
+	public function postCustomers($customers)
+	{
+		$countCustomers = count($customers);
+		if($countCustomers == 0)
+		{
+			// no customers = no email
+			return;
+		}
+
+		$email = $countCustomers . ' new customers registered in Paype, you can manage them at <a href="https://business.paype.com">business management website</a><br><br>';
+
+		$email .= '<table><tr>
+					<th>First name</th>
+					<th>Last name</th>
+					<th>Email</th>
+					<th>Gender</th>
+					<th>Birthday</th>
+					<th>Customer ID</th>
+				</tr>';
+
+		foreach($customers as $c)
+		{
+			$email .= '<tr>
+					<td>'.$c->first_name.'</td>
+					<td>'.$c->last_name.'</td>
+					<td>'.$c->email.'</td>
+					<td>'.$c->gender.'</td>
+					<td>'.$c->birthday.'</td>
+					<td>'.$c->customer_id.'</td>
+				</tr>';
+		}
+
+		$email .= '</table>';
+
+		foreach($this->emails as $mail)
+		{
+			mail($mail, 'New Paype users', $email);
+		}
+	}
+}
