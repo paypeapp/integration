395651269a5459013ef866f8560e9f0eadc750da
robertpallas <robert.pallas@igame.com>
2015-02-26 17:50:39 +0200

    Navision2 calls update

--- wsInterfaces/navision2.php
+++ wsInterfaces/navision2.php
@@ -4,62 +4,80 @@ class Navision2 extends Navision
 {
     public function getCustomers($lastSyncCustomerId)
     {
-        $returnCustomers = array();
+		return array(); // TODO: not yet syncing everything
+		$returnCustomers = array();
 
-        // TODO
+		$read = array(
+			'filter' => array(
+				array('Field'=>'Paype_ID', 'Criteria'=>'') // only ask ones with empty Paype ID - not synced yet
+			),
+			'bookmarkKey' => null,
+			'setSize' => 1000
+		);
 
-        return $returnCustomers;
+		if(!empty($lastSyncCustomerId))
+		{
+			$read['filter'][] = array('Field'=>'No', 'Criteria'=>$lastSyncCustomerId . '..'); // start from last synced ID
+		}
+
+		try
+		{
+			$customers = $this->client->ReadMultiple($read);
+		}
+		catch(Exception $e)
+		{
+			paypeLog('navision customersPush customer read fail: ' . $e->getMessage(), true);
+		}
+
+		if(empty($customers->ReadMultiple_Result->Customers))
+		{
+			$customers = array();
+		}
+		else
+		{
+			$customers = $customers->ReadMultiple_Result->Customers;
+		}
+
+		if(!is_array($customers))
+		{
+			$customers = array($customers);
+		}
+
+		foreach($customers as $customer)
+		{
+			if(!empty($customer->No))
+			{
+				// TODO: split first and last name
+				$returnCustomers[] = array(
+					'first_name' => (!empty($customer->Name) ? $customer->Name : ''),
+					'last_name' => (!empty($customer->Name) ? $customer->Name : ''),
+					'email' => $customer->E_Mail,
+					'customer_id' => $customer->Paype_ID
+				);
+			}
+		}
+
+		return $returnCustomers;
     }
 
-    public function postCustomers($customers)
-    {
-        foreach($customers as $c)
-        {
-            $create = array();
-            $create['Customers']['E_Mail'] = $c->email;
-            $create['Customers']['Name'] = $c->first_name . ' ' . $c->last_name;
-            $create['Customers']['IC_Partner_Code'] = $c->customer_id;
+	public function postCustomers($customers)
+	{
+		foreach($customers as $c)
+		{
+			$create = array();
+			$create['_Email'] = $c->email;
+			$create['_Name'] = $c->first_name . ' ' . $c->last_name;
+			$create['customerID'] = $c->customer_id;
 
-            try
-            {
-                $this->client->Create($create);
-            }
-            catch(Exception $e)
-            {
-                preg_match('/Same e-mail has already used in Regular Customer (.*)/', $e->getMessage(), $navId);
-
-                if(count($navId) == 2)
-                {
-                    // The error message contains RegularCustomer->No, call update on it
-                    $navNo = $navId[1];
-                    $this->updateCustomer($create, $navNo);
-                }
-                else
-                {
-                    paypeLog('navision customerPull create fail: ' . $e->getMessage(), true);
-                }
-            }
-        }
-    }
-
-    protected function updateCustomer($customer, $navNo)
-    {
-        try
-        {
-            $read = array();
-            $read['No'] = $navNo;
-
-            // get customer key by their NAV No from create error message
-            $navClient = $this->client->Read($read);
-
-            // update customer sending in create object with key received from read call
-            $customer['Customers']['Key'] = $navClient->Customers->Key;
-            $update = $this->client->Update($customer);
-            paypeLog('navision customerPull customer update: ' . json_encode($update));
-        }
-        catch(Exception $e)
-        {
-            paypeLog('navision customerPull update fail: ' . $e->getMessage(), true);
-        }
-    }
+			try
+			{
+				$debug = $this->client->CreateOrUpdateCustomer($create);
+				paypeLog('debug: ' . json_encode($debug) . ' '. json_encode($create));
+			}
+			catch(Exception $e)
+			{
+				paypeLog('navision customerPull create fail: ' . $e->getMessage(), true);
+			}
+		}
+	}
 }
