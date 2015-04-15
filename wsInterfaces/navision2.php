190a38be9cc9c98e2025aaadff9dc95af3f3d28b
robertpallas <robert.pallas@igame.com>
2015-04-15 19:32:42 +0300

    navision update

--- wsInterfaces/navision2.php
+++ wsInterfaces/navision2.php
@@ -68,18 +68,51 @@ class Navision2 extends Navision
 			$create['_Email'] = $c->email;
 			$create['_Name'] = $c->first_name . ' ' . $c->last_name;
 			$create['_Phone'] = $c->phone_international;
-			$create['customerID'] = $c->customer_id;
+			$create['_UpdateDiscount'] = false;
+
 			$create['_CustomerNo'] = '';
-			$create['_UpdateDiscount'] = $c->identification_count > 0;
+			$create['customerID'] = '';
+
+			if(Library::validateEstonianPersonalCode($c->customer_id))
+			{
+				$create['customerID'] = $c->customer_id;
+			}
+			elseif(!empty($c->personal_code))
+			{
+				$create['customerID'] = $c->personal_code;
+			}
+			else
+			{
+				$create['_CustomerNo'] = $c->customer_id;
+			}
+
+			//paypeLog('debug create > '. json_encode($create) . ' ' . json_encode($c));
 
 			try
 			{
-				$debug = $this->client->CreateOrUpdateCustomer($create);
-				paypeLog('debug: ' . json_encode($debug) . ' '. json_encode($create));
-			}
-			catch(Exception $e)
-			{
+				$createReturn = $this->client->CreateOrUpdateCustomer($create);
+				paypeLog('CreateOrUpdateCustomer: ' . json_encode($createReturn) . ' ' . json_encode($create));
+
+				if(!$createReturn->return_value)
+				{
+					// new customer
+					$this->sendMail($c);
+				}
+				elseif($c->identification_count > 0)
+				{
+					// old customer, and has identified
+					$create['_UpdateDiscount'] = true;
+					$updateDiscount = $this->client->CreateOrUpdateCustomer($create);
+					paypeLog('updateDiscount: ' . json_encode($updateDiscount));
+				}
+			} catch (Exception $e) {
 				paypeLog('navision customerPull create fail: ' . $e->getMessage(), true);
 			}
 		}
+	}
+
+	private function sendMail($customer)
+	{
+		// TODO
+	}
 }
