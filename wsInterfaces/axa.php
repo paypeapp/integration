0ecd2c96cbfc75761321cd5e5cc16d0d67ac7436
robertpallas <robert.pallas@igame.com>
2015-05-05 15:24:44 +0300

    Axa with metadata address

--- wsInterfaces/axa.php
+++ wsInterfaces/axa.php
@@ -17,16 +17,47 @@ class Axa implements WsInterface
 
 	public function postCustomers($customers)
 	{
+		/*$axaXml = '<?xml version="1.0" encoding="UTF-8"?><E-Document><Document>';*/
+
 		foreach($customers as $c)
 		{
+			$axaXml = '<?xml version="1.0" encoding="UTF-8"?><E-Document><Document>';
+
+			$address = json_decode($c->meta_data);
+
 			$axaCustomer = array();
 			$axaCustomer['Name'] = $c->first_name;
 			$axaCustomer['Surname'] = $c->last_name;
 			$axaCustomer['RegNum'] = $c->customer_id;
-			$axaCustomer['Phone'] = $c->phone_international;
-			$axaCustomer['EmailAddress'] = $c->email;
+			$axaCustomer['ContactData'] = array(
+				'Phone' => $c->phone_number,
+				'PhoneCountryCode' => $c->phone_country,
+				'EmailAddress' => $c->email,
+				'Address' => array(
+					'Country' => 'Eesti',
+					'CountryCode' => 'EE',
+					'County' => (!empty($address->county)?$address->county:''),
+					'Street' => (!empty($address->street)?$address->street:''),
+					'City' => (!empty($address->city)?$address->city:''),
+					'PostalCode' => (!empty($address->zip)?$address->zip:''),
+					'HouseNo' => (!empty($address->house)?$address->house:''),
+					'ApartmentNo' => (!empty($address->apartment)?$address->apartment:'')
+				)
+			);
 
-			$response = $this->client->__soapCall('CreateClient', array(array('xml'=>$this->arrayToXml($axaCustomer))));
+			$axaCustomer['AcceptsDataProcessing'] = 'Jah';
+			$axaCustomer['AcceptsSpecialOffers'] = 'Jah';
+			$axaCustomer['PaypeId'] = $c->token;
+			$axaCustomer['CustAccount'] = null;
+			$axaCustomer['VATRegNum'] = null;
+			$axaCustomer['Currency'] = 'EUR';
+			$axaCustomer['CustType'] = 'ERAISIK';
+
+			$axaXml .= '<DocumentCust>' . $this->arrayToXml($axaCustomer) . '</DocumentCust>';
+
+			$axaXml .= '</Document></E-Document>';
+
+			$response = $this->client->__soapCall('CallXMLAction', array(array('action'=>'createClient_MCC','xml'=>$axaXml)));
 
 			paypeLog('axa res > ' . json_encode($response));
 		}
@@ -37,6 +68,10 @@ class Axa implements WsInterface
 		$xml = '';
 		foreach($array as $key=>$value)
 		{
+			if(is_array($value))
+			{
+				$value = $this->arrayToXml($value);
+			}
 			$xml .= '<'.$key.'>'.$value.'</'.$key.'>';
 		}
 
